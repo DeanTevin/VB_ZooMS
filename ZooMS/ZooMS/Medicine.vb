1267e9ed390d2b102e0ea6d2b131f2c66c70b3d8
DeanTevin <Deantevinn@gmail.com>
2018-12-24 13:54:36 +0700

    All Medicine Functions added and bugfixed

--- ZooMS/ZooMS/Medicine.vb
+++ ZooMS/ZooMS/Medicine.vb
@@ -4,6 +4,7 @@ Imports ZooMS.koneksi
 
 Public Class Medicine
     Dim conn As New MySqlConnection("Server=localhost; user=root; database=zooms")
+    Dim conn2 As New MySqlConnection("Server=localhost; user=root; database=zooms")
     Dim CMD As New MySqlCommand
     Dim data As New MySqlDataAdapter
     Dim table As New DataTable()
@@ -12,23 +13,42 @@ Public Class Medicine
     Dim reader As MySqlDataReader
     Dim stock, qty, Result As Integer
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Conn.Open()
-        Try
-            CMD.CommandType = CommandType.Text
-            CMD.CommandText = "INSERT INTO medicine (MedicineName,Stock) VALUES('" & NewMedName.Text & "','" & NewMedStock.Text & "')"
-            CMD.Connection = conn
-            CMD.ExecuteNonQuery()
-            MsgBox("New Data Inserted", MsgBoxStyle.Information, "information")
-        Catch ex As Exception
-            MsgBox("New Data insert failed", MsgBoxStyle.Information, "information")
-        End Try
-        conn.Close()
+    Private Sub MedicineNew(sender As Object, e As EventArgs) Handles Button1.Click
+        If NewMedName.Text.Equals("") And NewMedStock.Text.Equals("") Then
+            MsgBox("Please Fill Medicine Name and Stock", MsgBoxStyle.Information, "information")
+        Else
+            conn.Open()
+            conn2.Open()
+            Try
+                CMD.CommandType = CommandType.Text
+                CMD.CommandText = "select * from medicine where MedicineName='" & NewMedName.Text & "'"
+                CMD.Connection = conn
+                reader = CMD.ExecuteReader()
+
+                If reader.HasRows Then
+                    MsgBox("Name already exists!", MsgBoxStyle.Information, "information")
+                Else
+                    Try
+                        CMD.CommandType = CommandType.Text
+                        CMD.CommandText = "INSERT INTO medicine (MedicineName,Stock) VALUES('" & NewMedName.Text & "','" & NewMedStock.Text & "')"
+                        CMD.Connection = conn2
+                        CMD.ExecuteNonQuery()
+                        MsgBox("New Data Inserted", MsgBoxStyle.Information, "information")
+                    Catch ex As Exception
+                        MsgBox("New Data insert failed", MsgBoxStyle.Information, "information")
+                    End Try
+                End If
+            Catch ex As Exception
+                MsgBox("No Connection", MsgBoxStyle.Information, "information")
+            End Try
+            conn.Close()
+            conn2.Close()
+        End If
     End Sub
 
     Private Sub Medicine_Load(sender As Object, e As EventArgs) Handles TabControl1.Click
         ComboBox1.Items.Clear()
-        ComboBox1.Items.Clear()
+        ComboBox2.Items.Clear()
         conn.Open()
         Try
             CMD.CommandType = CommandType.Text
@@ -87,6 +107,61 @@ Public Class Medicine
             End Try
             conn.Close()
         End If
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        conn.Open()
+        Try
+            CMD.CommandType = CommandType.Text
+            CMD.CommandText = "select * from medicine where MedicineName='" & ComboBox2.Text & "'"
+            CMD.Connection = conn
+            reader = CMD.ExecuteReader()
+
+            While reader.Read
+                TextBox6.Text = reader.GetString("Stock")
+            End While
+
+        Catch ex As Exception
+            MsgBox("No Data Found", MsgBoxStyle.Information, "information")
+        End Try
+        conn.Close()
+    End Sub
+
+    Private Sub MedicineAdd(sender As Object, e As EventArgs) Handles Button4.Click
+        qty = Convert.ToInt32(TextBox3.Text)
+        stock = Convert.ToInt32(TextBox4.Text)
+        Result = stock + qty
+        If Result < 0 Then
+            MsgBox("Quantity taken cannot exceeds stock, Transaction Failed", MsgBoxStyle.Information, "information")
+        Else
+            conn.Open()
+            Try
+                CMD.CommandType = CommandType.Text
+                CMD.CommandText = "UPDATE medicine set Stock=" & Result & " WHERE MedicineName='" & ComboBox1.Text & "'"
+                CMD.Connection = conn
+                CMD.ExecuteNonQuery()
+                TextBox4.Text = Result.ToString
+                MsgBox("Transaction Success", MsgBoxStyle.Information, "information")
+            Catch ex As Exception
+                MsgBox("Transaction Failed", MsgBoxStyle.Information, "information")
+            End Try
+            conn.Close()
+        End If
+    End Sub
+
+    Private Sub DeleteMedicine(sender As Object, e As EventArgs) Handles Button6.Click
+        conn.Open()
+        Try
+            CMD.CommandType = CommandType.Text
+            CMD.CommandText = "Delete from medicine WHERE MedicineName='" & ComboBox2.Text & "'"
+            CMD.Connection = conn
+            CMD.ExecuteNonQuery()
+            TextBox4.Text = Result.ToString
+            MsgBox("Deletion Success", MsgBoxStyle.Information, "information")
+        Catch ex As Exception
+            MsgBox("No Data to Delete", MsgBoxStyle.Information, "information")
+        End Try
+        conn.Close()
     End Sub
 
     Private Sub TextBox3_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox3.KeyPress
